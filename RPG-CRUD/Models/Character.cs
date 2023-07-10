759e5adc681c660cda591377633f9412c1713504
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-07-10 11:44:11 -0300

    first commit

---
+++ RPG-CRUD/Models/Character.cs
@@ -0,0 +1,7 @@
+namespace RPG_CRUD.Models;
+
+public class Character
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ RPG-CRUD/Models/Player.cs
@@ -0,0 +1,10 @@
+namespace RPG_CRUD.Models;
+
+public class Player
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public int HitPoints { get; set; }
+    public int Experience { get; set; }
+}
